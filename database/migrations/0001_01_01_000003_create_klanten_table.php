b575a9e088741773980a6fc292a19dcc3c538535
TimSchoot <timschoot62@gmail.com>
2025-09-18 13:19:39 +0200

    database

---
+++ database/migrations/0001_01_01_000003_create_klanten_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('klanten', function (Blueprint $table) {
+            $table->id();
+            $table->integer('klant_nummer')->unique();
+            $table->string('naam');
+            $table->string('email');
+            // $table->string('telefoon', 15)->nullable();
+            $table->string('straat_postcode')->nullable();
+            $table->string('plaats')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
